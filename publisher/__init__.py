752f2b17de0fe56f8020b78610ab613b6043901e
Uday Mittal <uday19@gmail.com>
2016-05-26 18:03:35 -0700

    Dynamic topic binding on the front end. Link lookup to publisher backend

--- publisher/__init__.py
+++ publisher/__init__.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import requests
+import json
 app = Flask(__name__)
 
-@app.route('/hello')
-@app.route('/hello/<name>')
-def hello_world(name=None):
-    return render_template('publish-ad.html', name=name)
+lookupaddr = 'http://0.0.0.0:5000'
+
+@app.route('/')
+@app.route('/publish/')
+def publish():
+    latitude = request.args.get('lat')
+    longitude = request.args.get('lon')
+
+    # request for region of the advertiser
+    r = requests.get(lookupaddr+'/region?lat='+str(latitude)+'&lon='+str(longitude))
+    publishregion = r.content
+    app.logger.debug("Region of publisher: " + str(publishregion))
+
+    # request for list of available topics
+    r = requests.get(lookupaddr+'/topiclist')
+    topiclist = json.loads(r.content)
+    for topic in topiclist:
+        app.logger.debug("Topic: " + str(topic))
+
+    return render_template('publish-ad.html', topiclist=topiclist)
 
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0")
+    app.debug=True
+    app.run(host="0.0.0.0", port=5100)
