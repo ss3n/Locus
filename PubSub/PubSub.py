b791b380e9650c5300c138d7270c58b288d03f5f
shiladitya <shiladis@uci.edu>
2016-05-26 23:42:00 -0700

    Modification of PubSub.publish

--- PubSub/PubSub.py
+++ PubSub/PubSub.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import json
 import KafkaAPI
 
 
@@ -10,19 +11,26 @@ def hello_world():
     return 'Hello World!'
 
 
-@app.route('/publish/', methods=['POST'])
+@app.route('/publish/', methods=['GET', 'POST'])
 def publish():
-    topic = request.form['topic']
-    message = request.form['ad']
-    KafkaAPI.publish(topic, message)
+    ad = json.loads(request.get_json())
+    topics = ad['topiclist']
+    message = ad['content']
+
+    app.logger.debug(topics)
+    app.logger.debug(message)
+
+    KafkaAPI.publish(topics, message)
+    return 'PUBLISHED!'
 
 
-@app.route('/subscribe/', methods=['GET'])
-def subscribe():
-    topic = request.form['topic']
-    subscription = KafkaAPI.subscribe(topic)
-    return subscription
+# @app.route('/subscribe/', methods=['GET'])
+# def subscribe():
+#     topic = request.form['topic']
+#     subscription = KafkaAPI.subscribe(topic)
+#     return subscription
 
 
 if __name__ == '__main__':
-    app.run()
+    app.debug = False
+    app.run(host='0.0.0.0', port=5000)
